91aace80ab8db51a9bb3d7be46d55d71ce49d526
Tendani2000 <127980784+Tendani2000@users.noreply.github.com>
2023-05-02 13:49:58 +0200

    IWA

---
+++ IWA_02 Challenge Solutions/IWA_2.4_Challenge_1_Solution.js
@@ -0,0 +1,17 @@
+// This is the firstname of the user
+
+const user = 'John'
+
+
+// This is the lastname of the user
+
+const surname = 'Smith'
+
+
+console.log(user, surname);
+
+
+/*Why did the original code not work?
+Because the whole code was covered by the
+comment tag. Semi-colons were in the wrong place
+and a comma was missing in the console log*/
