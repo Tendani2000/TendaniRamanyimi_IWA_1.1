36d7b9d4698769100c9e1dcbab65a9f19ad32100
Tendani2000 <127980784+Tendani2000@users.noreply.github.com>
2023-05-05 11:21:31 +0200

    IWA_4

--- IWA_03 Challenge Solutions/johannes.js
+++ IWA_03 Challenge Solutions/johannes.js
@@ -1,6 +1,6 @@
 const firstname = "Johannes"
 const surname = "Potgieter"
-const role = "Intern"
+let role = "Intern"
 
 const display= firstname + " " + surname + " (" + role + ")"
 document.querySelector('#johannes').innerText = display
