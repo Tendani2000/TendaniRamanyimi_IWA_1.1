120fe5a7b2b37e0ae17f95ba3f7e67d895fc1fab
Tendani2000 <127980784+Tendani2000@users.noreply.github.com>
2023-05-19 20:44:52 +0200

    IWA_9

--- IWA_7/IWA_7.5_Challenge_3.js
+++ IWA_7/IWA_7.5_Challenge_3.js
@@ -12,9 +12,9 @@ const divider = '----------------------------------'
 
 
 const owed = parseFloat (Math.abs(leoBalance)) + parseFloat (Math.abs( sarahBalance));
-const leo = `${leoName} ${leoSurname} Owed: R${parseFloat(Math.abs(sarahBalance)).toFixed(2)}`;
+const leo = `${leoName}${leoSurname}Owed: R${parseFloat(Math.abs(sarahBalance)).toFixed(2)}`;
 const sarah = `${sarahName} ${sarahSurname} Owed: R ${parseFloat(Math.abs(sarahBalance)).toFixed(2)}`;
 const total = "Total amount owed: R" +owed.toFixed(2)
-const result = "\n" + leo + "\n" + sarah + "\n" + divider + "\n" + total + "\n"+ divider;
+const result = "\n" + leo + "\n" + sarah + "\n" +  "\n" + divider + "\n" + total + "\n"+ divider;
 
 console.log(result)
